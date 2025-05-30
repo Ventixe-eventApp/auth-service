be422869b21bef8b0c131b5ffdfbbef6978f4198
Felicia Dahl <feliciadahl01@gmail.com>
2025-05-30 15:11:30 +0200

    swagger only fro dev

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -12,14 +12,16 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 
 
 var app = builder.Build();
-
-app.UseSwagger();
-app.UseSwaggerUI(c =>
+if (app.Environment.IsDevelopment())
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Event API V1");
-    c.RoutePrefix = string.Empty;
-});
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Event API V1");
+        c.RoutePrefix = string.Empty;
+    });
 
+}
 app.MapOpenApi();
 app.UseHttpsRedirection();
 app.UseCors(x => x
